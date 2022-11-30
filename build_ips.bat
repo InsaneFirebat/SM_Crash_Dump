667e4fec70150d47617cb2a581d0e6e6bc4373f4
InsaneFirebat <InsaneFirebat@gmail.com>
2022-11-29 22:27:22 -0500

    Enable extra pages for debug_extra.ips

--- build_ips.bat
+++ build_ips.bat
@@ -8,13 +8,13 @@ echo Building single-page version
 copy *.sfc ..\build
 asar\asar.exe --no-title-check -DEXTRA_PAGES=0 ..\CrashHandler.asm ..\build\00.sfc
 asar\asar.exe --no-title-check -DEXTRA_PAGES=0 ..\CrashHandler.asm ..\build\ff.sfc
-python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\debug_0.ips
+python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\debug.ips
 
 echo Building multi-page version
 copy *.sfc ..\build
-..\tools\asar\asar.exe --no-title-check -DEXTRA_PAGES=0 ..\CrashHandler.asm ..\build\00.sfc
-..\tools\asar\asar.exe --no-title-check -DEXTRA_PAGES=0 ..\CrashHandler.asm ..\build\ff.sfc
-python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\debug_1.ips
+..\tools\asar\asar.exe --no-title-check -DEXTRA_PAGES=1 ..\CrashHandler.asm ..\build\00.sfc
+..\tools\asar\asar.exe --no-title-check -DEXTRA_PAGES=1 ..\CrashHandler.asm ..\build\ff.sfc
+python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\debug_extra.ips
 
 del 00.sfc ff.sfc ..\build\00.sfc ..\build\ff.sfc
 cd ..
